d296a82f53825bbe9412db9201bd7f033e7f6bd5
Bziluch <bziluch@gmail.com>
2024-10-29 22:24:37 +0100

    feat: currency date field

--- assets/js/components/ExchangeRates.js
+++ assets/js/components/ExchangeRates.js
@@ -1,49 +1,60 @@
 import React, {Component} from "react";
 import axios from "axios";
+import {getBaseUrl} from "../tools/UrlFunctions";
+import {getCurrentDate} from "../tools/DateFunctions";
 
 class ExchangeRates extends Component {
 
     constructor() {
         super();
-        this.state = { currencies: {}, loading: true, date: null};
-    }
-
-    getBaseUrl() {
-        return 'http://127.0.0.1:8000';
+        this.state = { currencies: {}, loading: true, date: getCurrentDate()};
     }
 
     loadCurrencies() {
-        axios.get(this.getBaseUrl() + `/api/exchange-rates`).then(response => {
+        const url = getBaseUrl() + `/api/exchange-rates` + (this.state.date ? `?date=` + this.state.date : '')
+        axios.get(url).then(response => {
             this.setState({ currencies: Object.keys(response.data).map((key) => response.data[key]), loading: false})
-            console.log(this.state.currencies)
         }).catch(function (error) {
             console.error(error);
             this.setState({loading: false});
         });
     }
 
+    updateDate(val) {
+        this.setState({ date: val, loading: true})
+        this.loadCurrencies()
+    }
+
     componentDidMount() {
+
         this.loadCurrencies();
     }
 
     render() {
         const loading = this.state.loading;
         const currencies = this.state.currencies;
+        const date = this.state.date;
+
+        const handleDateChange = (e) => {
+            this.updateDate(e.target.value)
+        }
+
         return (
             <div>
                 <h2>Kursy walut</h2>
+                <input type={'date'} min={'2023-01-01'} value={date} onChange={handleDateChange}/>
                 <table className={'table'}>
                     <thead>
                     <tr>
-                    <th>Symbol</th>
-                            <th>Nazwa</th>
-                            <th>Kurs średni</th>
-                            <th>Kurs kupna</th>
-                            <th>Kurs sprzedaży</th>
-                            <th>Kurs średni z dzisiaj</th>
-                            <th>Kurs kupna z dzisiaj</th>
-                            <th>Kurs sprzedaży z dzisiaj</th>
-                        </tr>
+                        <th>Symbol</th>
+                        <th>Nazwa</th>
+                        <th>Kurs średni z {date}</th>
+                        <th>Kurs kupna z {date}</th>
+                        <th>Kurs sprzedaży z {date}</th>
+                        <th>Kurs średni z dzisiaj</th>
+                        <th>Kurs kupna z dzisiaj</th>
+                        <th>Kurs sprzedaży z dzisiaj</th>
+                    </tr>
                     </thead>
                     <tbody>
                         {loading ? (
@@ -73,7 +84,6 @@ class ExchangeRates extends Component {
             </div>
         )
     }
-
 }
 
 export default ExchangeRates;
